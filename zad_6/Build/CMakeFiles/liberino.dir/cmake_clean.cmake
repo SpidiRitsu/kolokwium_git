631e24591db6ab8bd4a8d707a92ec893c9aed703
Daniel <daniel@barcoding.software>
2020-01-16 09:30:42 +0100

    Zad 6

---
+++ zad_6/Build/CMakeFiles/liberino.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/liberino.dir/src/libtest.cpp.o"
+  "libliberino.pdb"
+  "libliberino.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/liberino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
